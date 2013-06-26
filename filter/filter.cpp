cb9508d4ae56c1bea3338bde586ada7e7aa4332d
Xitong Liu <foremire@gmail.com>
2013-06-26 01:37:16 +0000

    update namespace and fix ^M issue

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -26,9 +26,7 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/program_options.hpp>
 
-using namespace std;
-using namespace boost;
-using namespace boost::filesystem;
+//using namespace boost;
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
@@ -38,59 +36,59 @@ namespace po = boost::program_options;
 using namespace streamcorpus;
 
 int main(int argc, char **argv) {
+  std::clog << "Starting program" << std:: endl;
+  bool negate(false);
 
-    clog << "Starting program" <<endl;
-    bool negate(false);
+  // Supported options.
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "help message");
 
-    // Supported options.
-    po::options_description desc("Allowed options");
-    desc.add_options()
-    ("help,h", "help message")
-    ;
+  // Parse command line options
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
 
-    // Parse command line options
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
 
-    if (vm.count("help")) {
-        cout << desc << "\n";
-        return 1;
+  // Setup thrift reading and writing from stdin and stdout
+  int input_fd = 0;
+  int output_fd = 1;
+
+  // initialize input stream
+  boost::shared_ptr<TFDTransport> innerTransportInput(new TFDTransport(input_fd));
+  boost::shared_ptr<TBufferedTransport> transportInput(
+        new TBufferedTransport(innerTransportInput));
+  boost::shared_ptr<TBinaryProtocol> protocolInput(
+      new TBinaryProtocol(transportInput));
+  transportInput->open();
+
+  // initialize output stream
+  boost::shared_ptr<TFDTransport> transportOutput(new TFDTransport(output_fd));
+  boost::shared_ptr<TBinaryProtocol> protocolOutput(new TBinaryProtocol(transportOutput));
+  transportOutput->open();
+
+  // Read and process all stream items
+  StreamItem stream_item;
+  int cnt=0;
+
+  while (true) {
+    try {
+      // Read stream_item from stdin
+      stream_item.read(protocolInput.get());
+      std::cout << stream_item.stream_id << " clean_visible size: ";
+      std::cout << stream_item.body.clean_visible.size() << std::endl;
+      ++cnt;
     }
-
-    // Setup thrift reading and writing from stdin and stdout
-    int input_fd = 0;
-    int output_fd = 1;
-
-    // input
-    shared_ptr<TFDTransport> innerTransportInput(new TFDTransport(input_fd));
-    shared_ptr<TBufferedTransport> transportInput(new TBufferedTransport(innerTransportInput));
-    shared_ptr<TBinaryProtocol> protocolInput(new TBinaryProtocol(transportInput));
-    transportInput->open();
-
-    // output 
-    shared_ptr<TFDTransport> transportOutput(new TFDTransport(output_fd));
-    shared_ptr<TBinaryProtocol> protocolOutput(new TBinaryProtocol(transportOutput));
-    transportOutput->open();
-
-    // Read and process all stream items
-    StreamItem stream_item;
-    int cnt=0;
-
-    while (true) {
-        try {
-            // Read stream_item from stdin
-            stream_item.read(protocolInput.get());
-	        cout << stream_item.stream_id << " clean_visible size: " << stream_item.body.clean_visible.size() << endl;
-	        cnt++;
-        }
-        catch (TTransportException e) {
-            // Vital to flush the buffered output or you will lose the last one
-            transportOutput->flush();
-            clog << "Total stream items processed: " << cnt << endl;
-            break;
-        }
+    catch (TTransportException e) {
+      // Vital to flush the buffered output or you will lose the last one
+      transportOutput->flush();
+      std::clog << "Total stream items processed: " << cnt << std::endl;
+      break;
     }
-    return 0;
+  }
+  return 0;
 }
 
